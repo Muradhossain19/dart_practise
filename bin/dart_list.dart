9407699f6dab31c3b501454e1d7831a9dc2f2a1e
Muradhossain19 <muradhossain.md1997@gmail.com>
2024-03-03 23:22:55 +0600

    update

--- bin/dart_list.dart
+++ bin/dart_list.dart
@@ -12,13 +12,24 @@ main() {
   studentList.add('Shamim'); // single kono data type value existing list add korte chyle
   studentList.addAll(['Murad','Rakib','Sabiha']); // existing list e aro ekta list ba multiple value add korte chayle
   print(studentList);
+
   //studentList = []; // List er item remove korte chayle ba pura ta empty korte chayle
   print(studentList);
   print(studentList[0]); // List er moddhe particuler item access ba print korate chyle index number soho dite hobe
   studentList.remove('Shamim'); // List er moddhe jokono ekta value k remove korte chayle, jodi ek e value 2 bar thake tahole first value ta delete hobe shudu
   print(studentList);
+  studentList.removeAt(2); // eta dara index remove kora jay
+  print(studentList);
+  studentList.clear(); // clear diye sob delete kora jabe
   studentList.removeWhere((item) => item == 'Murad'); // ek e value list er moddhe jotobar thakbe sob koytay remove kore dibe
   print(studentList);
+
+  studentList.insert(2, 'Shakil'); // insert diye koto tomo index er por insert/add korte chay seta kora jay
+  print(studentList);
+  studentList.insertAll(3, ['Pakhi', 'Titu', 'Pulok']); // insertAll diye koto tomo index er por new ekta list add kora jay
+  print(studentList);
+
+
   studentList.sort(); // accending korar jonno
   print(studentList);
 
