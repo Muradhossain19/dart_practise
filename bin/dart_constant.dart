43195c3a5bbc0f03964d597956eb825f05382ebf
Muradhossain19 <muradhossain.md1997@gmail.com>
2024-03-03 22:47:15 +0600

    update

--- bin/dart_constant.dart
+++ bin/dart_constant.dart
@@ -1,12 +1,29 @@
 // ignore_for_file: unused_local_variable
 
+
+
 main() {
   // constant data type declare korle tahole variable er value kono change kora jabe na ar.
   // constant er short hylo => const
+  // compile time constant
 
   const String myName = 'Pintu';
 
+  // myName = 'Zephyr'; // ekbar const declare korle variable value r change kora jabe nah
+  // myName = 'Shihab';
+
   print(myName);
+  
+  // Run time constant
+  // ekbar intialized kore dile next time r kora jabe nah
+  // final ta basically class er moddhe use kora hoy
+  final double result;
+
+  result = 3.50;
+  // result = 2.50;
+
+  print(result);
+
 
 
 
