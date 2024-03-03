322c4368141c8618199d3b00f296871b677ac6f5
Muradhossain19 <muradhossain.md1997@gmail.com>
2024-03-03 22:28:38 +0600

    update

---
+++ bin/dart_constant.dart
@@ -0,0 +1,13 @@
+// ignore_for_file: unused_local_variable
+
+main() {
+  // constant data type declare korle tahole variable er value kono change kora jabe na ar.
+  // constant er short hylo => const
+
+  const String myName = 'Pintu';
+
+  print(myName);
+
+
+
+}
