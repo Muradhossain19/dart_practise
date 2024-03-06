9e0fead3631117e84ad027a5579b8c7b14fbe752
Muradhossain19 <muradhossain.md1997@gmail.com>
2024-03-07 00:01:57 +0600

    update

---
+++ bin/dart_operators.dart
@@ -0,0 +1,37 @@
+main() {
+  // Arethmetic Operator
+  int a = 5 + 5; // Plus korar jonno
+  int b = 10 - 3; // Minus korar jonno
+  int c = 15 * 3; // Gun korar jonno
+  double d = 34 / 2; // Divide korar jonno
+  int e = 23 % 2; // Vagses ber korar jonno
+
+  print(a);
+  print(b);
+  print(c);
+  print(d);
+  print(e);
+
+  // Unary Operator
+  // Egula k bole postfix operators
+  // a = a + 1;
+  print(a++); // evabe dile porer line jeye plus hobe
+  print(a);
+  // a = a - 1;
+  print(a--); // evabe dile porer line jeye Minus hobe
+  print(a);
+  
+  // Egula k prefix bole
+  print(++a); // sathe sathey Plus Hoye jabe
+  print(--a); // sathe sathey Minus Hoye jabe
+  
+  // jodi 1 er odhik Plus,Minus,Multiply,Divided,Modulus korte chay
+  a += 2; // a = a + 2; jodi 
+  a -= 2; // a = a - 2;
+  a *= 2; // a = a * 2;
+  //a /= 2; // a = a / 2;
+  a %= 2; // a = a % 2; 
+
+
+
+}
