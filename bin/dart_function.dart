0b00ae640b8764e1a5064bdd9aa2b794eae43175
Muradhossain19 <muradhossain.md1997@gmail.com>
2024-03-08 00:28:27 +0600

    update

--- bin/dart_function.dart
+++ bin/dart_function.dart
@@ -1,5 +1,22 @@
 main() {
+   // function create korar por execute korar jonno main function er body te call korte hobe
+   welcomeMessage('Murad');
+   welcomeMessage('Rakib','Shologhar', 27);
+   welcomeMessage('Zephyr','Shologhar', 27);
+   welcomeMessage('Monir','Shologhar', 27);
+   welcomeMessage('Tutul','Shologhar', 27);
+   welcomeMessage('Dintu','Shologhar', 27);
+   
 
 
   
+}
+
+// Function
+// () etar vitore ki dhoroner paremeter nite chay oita boshate hobe
+// kono variable optional korar jonno [] use korte hoy
+welcomeMessage(String name, [String? address, int? age]){
+   print('Hi Good Morning $name $address $age');
+   print('How are you');
+
 }
