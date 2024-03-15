5d65fcf0d67e50827e8eff0973762ef0e2eceb44
Muradhossain19 <muradhossain.md1997@gmail.com>
2024-03-16 00:36:59 +0600

    update

---
+++ assignment1st/first_assignment.dart
@@ -0,0 +1,12 @@
+import 'class.dart';
+
+void main() {
+  Media media = Media();
+  Song song = Song("Artist Name");
+
+  media.play(); // Output: Playing media...
+  song.play(); // Output: Playing song by Artist Name...
+}
+
+
+
