643f3fec3dec073af1a391ac61dbfca44cff0e22
Muradhossain19 <muradhossain.md1997@gmail.com>
2024-04-28 11:21:38 +0600

    update

--- bin/exception_handling.dart
+++ bin/exception_handling.dart
@@ -2,3 +2,33 @@
 // remember this two keywords for exception handling
 // Try - Catch
 // Throws
+// try - catch - throw
+
+void main (){
+
+printer();
+
+
+}
+
+void printer(){
+  try{
+  printHeadline();
+  } catch (e){
+    print('Something Went Wrong : ${e.toString()}');
+  }
+  printContent();
+
+}
+
+
+void printHeadline(){
+  throw Exception('Not Printing');
+
+  // ignore: dead_code
+  print('Printing Headline');
+}
+
+void printContent(){
+  print('Printing Content');
+}
